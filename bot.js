d9ee28e5a6e7fcb4462ddb002c6bb16df987da08
THE GRID <34685278+Predvkill@users.noreply.github.com>
2019-02-04 21:41:10 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -270,21 +270,6 @@ msg.author.send(":notes: **RINZLER MUSIC BOT COMMANDS** :notes:" + `  **
  }
 });
 
-client.on('ready', function(){
-  client.user.setStatus("dnd");
-    var ms = 10000 ;
-    var setGame = ['★ -Play | MusicBot ★','The Grid™ | Server ' ];
-    var i = -1;
-    var j = 0;
-    setInterval(function (){
-        if( i == -1 ){
-            j = 1;
-        }
-        if( i == (setGame.length)-1 ){
-            j = -1;
-        }
-        i = i+j;
-        client.user.setGame(setGame[i],`https://www.twitch.tv/TheRealPredvkill`);
-    }, ms);
+
 
 client.login(process.env.BOT_TOKEN);
