f9b79ecc477576e8d6320d5283ee2501e9292ce6
THE GRID <34685278+Predvkill@users.noreply.github.com>
2019-02-26 21:35:02 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -76,45 +76,6 @@ client.on('ready', function(){//npm i ms
 
 });
 
-
-function timeCon(time) {
-    let days = Math.floor(time % 31536000 / 86400)
-    let hours = Math.floor(time % 31536000 % 86400 / 3600)
-    let minutes = Math.floor(time % 31536000 % 86400 % 3600 / 60)
-    let seconds = Math.round(time % 31536000 % 86400 % 3600 % 60)
-    days = days > 9 ? days : '0' + days
-    hours = hours > 9 ? hours : '0' + hours
-    minutes = minutes > 9 ? minutes : '0' + minutes
-    seconds = seconds > 9 ? seconds : '0' + seconds
-    return `${days > 0 ? `${days}:` : ''}${(hours || days) > 0 ? `${hours}:` : ''}${minutes}:${seconds}`
-}
-var version = '1.5';
-client.on('message', message => {
-    if (message.content.startsWith(prefix + "stats")) {
-    if(!message.channel.guild) return message.reply('** :x: This Command Only For Servers :x:**');
-    message.channel.send({
-        embed: new Discord.RichEmbed()
-            .setAuthor(client.user.username,client.user.avatarURL)
-            .setThumbnail(client.user.avatarURL)
-            .setColor('RANDOM')
-            .setTitle('``Rinzler STATS`` ')
-            .addField('``Uptime``', [timeCon(process.uptime())], true)
-            .addField('``My Ping``' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
-            .addField('``RAM Usage``', `[${(process.memoryUsage().rss / 1048576).toFixed()}MB]`, true)
-            .addField('``servers``', [client.guilds.size], true)
-            .addField('``channels``' , `[ ${client.channels.size} ]` , true)
-            .addField('``Users``' ,`[ ${client.users.size} ]` , true)
-            .addField('``My Name``' , `[ ${client.user.tag} ]` , true)
-            .addField('``My ID``' , `[ ${client.user.id} ]` , true)
-            .addField('``Node``' , `[${process.version} ]` , true)
-                  .addField('``My Prefix``' , `-` , true)
-                  .addField('``My Language``' , `[ Java Script ]` , true)
-                  .setFooter('By | TheRareRanger')
-    })
-}
-});
-
-
 client.on('message', async msg =>{
 	if (msg.author.bot) return undefined;
     if (!msg.content.startsWith(prefix)) return undefined;
