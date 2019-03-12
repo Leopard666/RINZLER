1ddc395a014aebbc624086c6612b6e35bd7b6f6b
THE GRID <34685278+Predvkill@users.noreply.github.com>
2019-03-12 20:34:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,13 +7,14 @@
 
 const client = new Discord.Client();
 const Discord = require('discord.js'),
+let cooldown = new Set();
+let cdseconds = 5;
+const prefix = "-";     
 YTDL = require("ytdl-core"),
 FFMPEG = require("ffmpeg"),
 YouTube = require('simple-youtube-api'),
 YTapi = new YouTube(process.env.ytapikey ? process.env.ytapikey : require("./config.json").ytapikey);
 prefix = process.env.prefix ? process.env.prefix : require("./config.json").prefix,
-let cooldown = new Set();
-let cdseconds = 5;
 
 // ==================================================================
 
