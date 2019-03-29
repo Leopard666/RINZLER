116b6b7a6b2a3fa905a33bc310370129b4547318
THE GRID <34685278+Predvkill@users.noreply.github.com>
2019-03-29 22:04:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -439,7 +439,7 @@ else if (mess.startsWith(prefix + 'skip')) {
 
         dispatcher.setVolume(1 * args / 50)
 	    
-        message.channel.sendMessage(`:loud_sound: | ``[${dispatcher.volume*50}]% : تم تغير مستوى الصوت`` | :loud_sound:`);
+        message.channel.sendMessage(':loud_sound: | ``[${dispatcher.volume*50}]% : تم تغير مستوى الصوت`` | :loud_sound:');
 
 	  message.channel.send({
 	    
