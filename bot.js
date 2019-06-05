527ba3271cfc42ae4272e77bae76d027cf318166
THE GRID™ - 2077 <34685278+Predvkill@users.noreply.github.com>
2019-06-05 06:28:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1036,6 +1036,50 @@ message.channel.send(embed);
 	    
 // ==================================================================
 
+client.on('message', message => {
+              if(!message.channel.guild) return;
+    var prefix = "-";
+    if(message.content.startsWith(prefix + 'bc')) {
+    if(!message.channel.guild) return message.channel.send('**الأمر بالسيرفرات بس**').then(m => m.delete(5000));
+  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**ماعندك الصلاحية المطلوبة**' );
+    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+    let copy = "🔰 THE GRID 🔰™";
+    let request = `Requested By ${message.author.username}`;
+    if (!args) return message.reply('**ترسل برودكاست فاضي؟**');message.channel.send(`**متأكد؟** \` ${args}\``).then(msg => {
+    msg.react('✅')
+    .then(() => msg.react('❌'))
+    .then(() =>msg.react('✅'))
+
+    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+
+    let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+    reaction1.on("collect", r => {
+    message.channel.send(`**☑ | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.size} Members**`).then(m => m.delete(5000));
+    message.guild.members.forEach(m => {
+    var bc = new
+       Discord.RichEmbed()
+       .setColor('RANDOM')
+       .setTitle('**:incoming_envelope:رسالة:incoming_envelope:')
+       .addField('**:diamond_shape_with_a_dot_inside:السيرفر:diamond_shape_with_a_dot_inside:**', message.guild.name)
+       .addField('**:black_joker:المرسل:black_joker:**', message.author.username)
+       .addField(':page_facing_up:الرسالة:page_facing_up:', args)
+       .setThumbnail(message.author.avatarURL)
+       .setFooter(copy, client.user.avatarURL);
+    m.send({ embed: bc })
+    msg.delete();
+    })
+    })
+    reaction2.on("collect", r => {
+    message.channel.send(`**كنسل يا مدير**`).then(m => m.delete(5000));
+    msg.delete();
+    })
+    })
+    }
+	
+// ==================================================================
+
                  client.login(process.env.BOT_TOKEN);
 
 // ==================================================================
